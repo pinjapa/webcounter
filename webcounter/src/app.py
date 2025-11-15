c97f81108c61c4f1c66df650177cdbaef2c22497
Pinja Paivakumpu <paivakumpupinja@gmail.com>
2025-11-15 13:53:43 +0200

    3: weblaskurin nollaus

--- webcounter/src/app.py
+++ webcounter/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from counter import Counter
 
 app = Flask(__name__)
@@ -16,4 +16,11 @@ def increment():
 @app.route("/reset", methods=["POST"])
 def decrement():
     cnt.reset()
+    return redirect("/")
+
+@app.route("/set", methods=["POST"])
+def set():
+    cnt.reset()
+    value = request.form["value"]
+    cnt.set_value(int(value))
     return redirect("/")
